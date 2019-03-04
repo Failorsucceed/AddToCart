3d92d8636487ae8f861b0d2422e0b10a009528d6
pyc <1214418375@qq.com>
2019-03-04 23:30:30 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+var app =new Vue({
+    el:'#app',
+    data:{
+        list:[
+            {
+                id: 1,
+                name: 'iphoneXS',
+                price: 9888,
+                count: 1
+            },
+            {
+                id:2,
+                name:'huawei',
+                price:5288,
+                count:1
+            },
+            {
+                id:3,
+                name:'xiaomi9',
+                price:3299,
+                count:1
+            }
+        ]
+
+    },
+    computed:{
+        totalPrice:function () {
+            var total=0;
+            for(var i=0;i<this.list.length;i++){
+                var item=this.list[i];
+                total +=item.price*item.count;
+            }
+            return total.toString().replace(/\B(?=(\d{3})+$)/g,',');
+        }
+    },
+    methods:{
+        handleReduce:function (index) {
+            if(this.list[index].count===1) return;
+            this.list[index].count--;
+        },
+        handleAdd:function (index) {
+            this.list[index].count++;
+        },
+        handleRemove:function (index) {
+            this.list.splice(index,1);
+        }
+    }
+});
